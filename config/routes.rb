66002dfc37fb68b0c33ba65e6d2d3d65116fb4b1
Renato Schneeberger <renato.schneeberger@iam-lab.ch>
2024-02-20 15:34:56 +0000

    task manager soulution

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new", as: "task_new"
+  post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit", as: "task_edit"
+  patch "tasks/:id", to: "tasks#update"
+  get "tasks/:id", to: "tasks#show", as: "task"
+  delete "tasks/:id", to: "tasks#destroy"
 end
